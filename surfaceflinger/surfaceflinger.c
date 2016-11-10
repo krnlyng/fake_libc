6abebb33d7f9b8d68109033033d4c44596ceefc5
Frajo Haider <f_haider@gmx.at>
2016-11-10 18:09:31 +0000

    add the surfaceflinger loader

---
+++ surfaceflinger/surfaceflinger.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <assert.h>
+
+#include <dlfcn.h>
+
+extern void *android_dlopen(const char *name, int flags);
+
+static void *surfaceflinger_handle = NULL;
+
+void start_surfaceflinger()
+{
+    int (*main_surfaceflinger)(int argc, char *argv[]) = NULL;
+
+    surfaceflinger_handle = android_dlopen("surfaceflinger.so", RTLD_LAZY);
+
+    assert(surfaceflinger_handle != NULL);
+
+    main_surfaceflinger = dlsym(surfaceflinger_handle, "main");
+
+    assert(main_surfaceflinger != NULL);
+
+    char *arg = "/system/bin/surfaceflinger";
+    char **argv = malloc(sizeof(char*));
+    *argv = arg;
+    main_surfaceflinger(1, argv);
+}
+
+int main(int argc, char *argv[])
+{
+    start_surfaceflinger();
+
+    return 0;
+}
+
